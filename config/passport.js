19429b41cc32f2f35d6ada5990f23804553e6238
LuisEJaar <luisjaarcoding@gmail.com>
2022-08-07 21:12:56 -0500

    Fixed issue thanks to some help

--- config/passport.js
+++ config/passport.js
@@ -32,20 +32,12 @@ module.exports = function(passport) {
         }
     }))
 
-    passport.serializeUser(function(user, cb) {
-      process.nextTick(function() {
-        return cb(null, {
-          id: user.id,
-          username: user.username,
-          picture: user.picture
-        });
-      });
+    passport.serializeUser((user, done) => {
+      done(null, user.id);
     });
-    
-    passport.deserializeUser(function(user, cb) {
-      process.nextTick(function() {
-        return cb(null, user);
-      });
+  
+    passport.deserializeUser((id, done) => {
+      User.findById(id, (err, user) => done(err, user));
     });
 
     // passport.serializeUser((user, done) => {
